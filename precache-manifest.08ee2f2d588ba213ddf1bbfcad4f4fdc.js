87ef763aa9b7e359818cdc9b307579e4e97a3ed4
luis1896 <luismartinezlopez1896@gmail.com>
2019-10-02 14:49:53 -0500

    Updates

---
+++ precache-manifest.08ee2f2d588ba213ddf1bbfcad4f4fdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c6a54fcca91c23887e8e5f31494235",
+    "url": "/demo-app/index.html"
+  },
+  {
+    "revision": "b3cf35a16d5ba2f0cce6",
+    "url": "/demo-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9de1fd919dff378f4171",
+    "url": "/demo-app/static/js/2.3755c5cd.chunk.js"
+  },
+  {
+    "revision": "b3cf35a16d5ba2f0cce6",
+    "url": "/demo-app/static/js/main.7aeea0e9.chunk.js"
+  },
+  {
+    "revision": "d7947cae8667204a1fb2",
+    "url": "/demo-app/static/js/runtime-main.8f07e98e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/demo-app/static/media/logo.25bf045c.svg"
+  }
+]);
